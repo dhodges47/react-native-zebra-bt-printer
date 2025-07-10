763b12a5342f85e32b55ae2784ea93dadbda4fa2
David Hodges <dhodges@gmail.com>
2025-07-10 08:26:55 -0400

    remove IOS; add config file for auto-linking

---
+++ react-native-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dependency: {
+    platforms: {
+      android: {
+        sourceDir: '../android',
+        packageImportPath: 'import com.atechticketing.TS_Mobile_RN_3.AtechBluetoothPackage;',
+        packageInstance: 'new AtechBluetoothPackage()'
+      }
+    }
+  }
+}; 
